076e008b3978da5a39f711ffb00f09838a65950e
Frederik Springer Krehan <s203684@student.dtu.dk>
2025-11-26 15:37:09 +0100

    prelimanry determin moodel

---
+++ src/deterministic_opt_model.py
@@ -0,0 +1,219 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+
+
+class Expando:
+    """A class that allows dynamic attribute assignment."""
+    pass
+
+class InputData:
+
+    def __init__(self,
+                 variables: list,
+                 gas_prices: list[float],
+                 coal_prices: list[float],
+                 eua_prices: list[float],
+                 rhs_demand: float,
+                 rhs_storage: dict[float],
+                 rhs_prod: dict[float],
+                 rhs_prod_wind: list[float],
+                 rhs_prod_pv: list[float],
+                 efficiencies: dict[float],
+                 co2_per_kWh: dict[float],
+                 min_prod_ratio: dict[float],
+                 starting_storage_levels: dict[float]):
+        
+        self.variables = variables
+        self.gas_prices = gas_prices
+        self.coal_prices = coal_prices
+        self.eua_prices = eua_prices
+        self.rhs_demand = rhs_demand
+        self.rhs_storage = rhs_storage
+        self.rhs_prod = rhs_prod
+        self.rhs_wind_prod = rhs_prod_wind
+        self.rhs_pv_prod = rhs_prod_pv
+        self.efficiencies = efficiencies
+        self.co2_per_kWh = co2_per_kWh
+        self.min_prod_ratio = min_prod_ratio
+        self.starting_storage_levels = starting_storage_levels
+        
+
+class DeterministicModel():
+
+    def __init__(self, input_data: InputData, name: str = "Deterministic Optimization Model", days: int = 366):
+        self.data = input_data
+        self.days = days
+        self.name = name
+        self.results = Expando()
+        self._build_model()
+
+    def _build_variables(self):
+        self.variables = [
+        [self.model.addVar(name=f"{v}_{t}") for t in self.days]
+        for v in self.data.variables
+    ]
+
+    def _build_constraints(self):
+
+        # Annual demand constraint
+        self.demand = self.model.addLConstr(
+            gp.quicksum(self.variables['P_COAL'][t] + self.variables['P_GAS'][t] + self.variables['P_WIND'][t] + self.variables['P_PV'][t] for t in self.days),
+                         GRB.GREATER_EQUAL, self.data.rhs_demand)
+    
+        # Storage maximum capacity constraints
+        self.storage_gas__max = [self.model.addLConstr(
+            self.variables['Q_GAS_STORAGE'][t], GRB.LESS_EQUAL, self.data.rhs_storage['Q_GAS_STORAGE']
+            )   
+            for t in self.days
+        ]
+        
+        self.storage_coal__max = [self.model.addLConstr(
+            self.variables['Q_COAL_STORAGE'][t], GRB.LESS_EQUAL, self.data.rhs_storage['Q_COAL_STORAGE']
+            )
+            for t in self.days
+        ]
+        
+        # Storage balance constraints
+        self.storage_gas = [self.model.addLConstr(
+             self.variables['Q_GAS_BUY'][t] + self.variables['Q_GAS_STORAGE'][t-1] - (self.variables['P_GAS'][t] / self.data.efficiencies['eta_GAS']), GRB.EQUAL, self.variables['Q_GAS_STORAGE'][t]
+            )
+            for t in range(1, self.days)
+        ]
+                        
+        
+        self.storage_coal = [self.model.addLConstr(
+             self.variables['Q_COAL_BUY'][t] + self.variables['Q_COAL_STORAGE'][t-1] - (self.variables['P_COAL'][t] / self.data.efficiencies['eta_COAL']), GRB.EQUAL, self.variables['Q_COAL_STORAGE'][t] 
+            )
+            for t in range(1, self.days)
+        ]
+        
+        # Initial storage level constraints
+        self.init_storage_gas = self.model.addLConstr(
+            self.variables['Q_GAS_BUY'][0] + self.data.starting_storage_levels['Q_GAS_STORAGE'] - (self.variables['P_GAS'][0] / self.data.efficiencies['eta_GAS']), GRB.EQUAL, self.variables['Q_GAS_STORAGE'][0]
+        )
+        
+        self.init_storage_coal = self.model.addLConstr(
+            self.variables['Q_COAL_BUY'][0] + self.data.starting_storage_levels['Q_COAL_STORAGE'] - (self.variables['P_COAL'][0] / self.data.efficiencies['eta_COAL']), GRB.EQUAL, self.variables['Q_COAL_STORAGE'][0]
+        )
+        
+        self.final_storage_gas = self.model.addLConstr(
+            self.variables['Q_GAS_STORAGE'][self.days - 1], GRB.EQUAL, self.data.starting_storage_levels['Q_GAS_STORAGE']
+        )
+        
+        self.final_storage_coal = self.model.addLConstr(
+            self.variables['Q_COAL_STORAGE'][self.days - 1], GRB.EQUAL, self.data.starting_storage_levels['Q_COAL_STORAGE']
+        )
+        # CO2 Emission
+        self.CO2_emission = self.model.addLConstr(
+            gp.quicksum(self.variables['P_GAS'][t] * self.data.co2_per_kWh['CO2_per_kWh_GAS'] + self.variables['P_COAL'][t] * self.data.co2_per_kWh['CO2_per_kWh_COAL'] for t in self.days), GRB.LESS_EQUAL, gp.quicksum(self.variables['Q_EUA'][t] for t in self.days)
+        )
+        
+        # Mqximum production constraints
+        self.max_prod_COAL_cap = [ self.model.addLConstr(
+            self.variables['P_COAL'][t], GRB.LESS_EQUAL, self.data.rhs_prod['P_COAL']
+        )
+        for t in self.days
+        ]
+        
+        self.max_prod_GAS_cap = [ self.model.addLConstr(
+            self.variables['P_GAS'][t], GRB.LESS_EQUAL, self.data.rhs_prod['P_GAS']
+        )
+        for t in self.days
+        ]
+        
+        # Maximum production constrainted by resource availability
+        self.max_prod_GAS = [ self.model.addLConstr(
+            self.variables['P_GAS'][t], GRB.LESS_EQUAL, self.efficiencies['eta_GAS'] * (self.variables['Q_GAS_STORAGE'][t-1] + self.variables['Q_GAS_BUY'][t])
+        ) 
+            for t in range(1, self.days)
+        ]
+        
+        self.max_prod_COAL = [ self.model.addLConstr(
+            self.variables['P_COAL'][t], GRB.LESS_EQUAL, self.efficiencies['eta_COAL'] * (self.variables['Q_COAL_STORAGE'][t-1] + self.variables['Q_COAL_BUY'][t])
+        ) 
+            for t in range(1, self.days)
+        ]
+        
+        # Initial maximum production constrainted by resource availability
+        self.max_prod_GAS_init = self.model.addLConstr(
+            self.variables['P_GAS'][0], GRB.LESS_EQUAL, self.efficiencies['eta_GAS'] * (self.data.starting_storage_levels['Q_GAS_STORAGE'] + self.variables['Q_GAS_BUY'][0])
+        )
+        
+        self.max_prod_COAL_init = self.model.addLConstr(
+            self.variables['P_COAL'][0], GRB.LESS_EQUAL, self.efficiencies['eta_COAL'] * (self.data.starting_storage_levels['Q_COAL_STORAGE'] + self.variables['Q_COAL_BUY'][0])
+        )
+        
+        self.max_prod_wind = [ self.model.addLConstr(
+            self.variables['P_WIND'][t], GRB.LESS_EQUAL, self.data.rhs_wind_prod[t]
+        )
+        for t in self.days
+        ]  
+        
+        self.max_prod_pv = [ self.model.addLConstr(
+            self.variables['P_PV'][t], GRB.LESS_EQUAL, self.data.rhs_pv_prod[t]
+        )
+        for t in self.days
+        ]
+        
+        
+        # Minimum production for coal and gas plants
+        self.min_prod_COAL = [ self.model.addLConstr(
+            self.variables['P_COAL'][t], GRB.GREATER_EQUAL, self.data.min_prod_ratio['min_prod_ratio_COAL'] * self.data.rhs_prod['P_COAL']
+        )
+        for t in self.days
+        ]
+        
+        self.min_prod_GAS = [ self.model.addLConstr(
+            self.variables['P_GAS'][t], GRB.GREATER_EQUAL, self.data.min_prod_ratio['min_prod_ratio_GAS'] * self.data.rhs_prod['P_GAS']
+        )
+        for t in self.days
+        ]
+        
+
+        ### All variables are automtically set to be greater than or equal to zero
+
+
+
+    def _build_objective(self):
+        self.model.setObjective(
+            gp.quicksum(self.variables['Q_GAS_BUY'][t] * self.data.gas_prices[t] + self.variables['Q_COAL_BUY'] * self.data.coal_prices + self.variables['Q_EUA'] * self.data.eua_prices for t in self.days), 
+            GRB.MINIMIZE)
+
+
+    def _build_model(self):
+        self.model = gp.Model(self.name)
+        self._build_variables()
+        self._build_constraints()
+        self._build_objective()
+        self.model.update()
+    
+    
+    def _save_results(self):
+        self.results.obj_val = self.model.ObjVal
+        self.results.var_vals = {(self.data.variables[v], t): self.variables[v][t].x
+                         for v in range(len(self.data.variables)) for t in self.days}
+        # please return the dual values for all constraints
+        # self.results.dual_vals = {
+        #     'upper_power': [self.upper_power[i].Pi for i in range(len(self.upper_power))],
+        #     'hourly_balance': [self.hourly_balance[i].Pi for i in range(len(self.hourly_balance))],
+        #     'daily_balance': self.daily_balance.Pi
+        # }
+
+    def run(self):
+        self.model.optimize()
+        if self.model.status == GRB.OPTIMAL:
+            self._save_results()
+        else:
+            raise ValueError("No optimal solution found for {self.model.name}")
+        
+    def display_results(self):
+        print()
+        print("-------------------   RESULTS  -------------------")
+        print("Optimal objective value:")
+        print(self.results.obj_val)
+        print("Optimal variable values:")
+        print(self.results.var_vals)
+        #print("Optimal dual values:")
+        #print(self.results.dual_vals)
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from data_preparation import DataPreparationCSV, DataPreparationJSON 
+from deterministic_opt_model import DeterministicModel, InputData
 
 
 # Load data files and prepare the dataset for timeseries
@@ -20,3 +21,60 @@ df_stor = params.storage_data_preparation()
 
 
 
+variables = df_app['DER_id'].tolist() + df_stor['storage_id'].tolist() + ['Q_COAL_BUY', 'Q_GAS_BUY', 'Q_EUA']
+coal_prices = df_t['Coal_Price[EUR/KWh]'].tolist()
+gas_prices = df_t['Gas_Price[EUR/KWh]'].tolist()
+eua_prices = df_t['ETS_Price[EUR/kgCO2eq]'].tolist()
+rhs_demand = 1000000000000 # Placeholder for demand RHS
+# Dictionary to map coal and gas to their respective storage capacities
+rhs_storage = {
+    'Q_COAL_STORAGE': df_stor.loc[df_stor['storage_id'] == 'Q_COAL_STORAGE', 'capacity_kWh_fuel'].values[0],
+    'Q_GAS_STORAGE': df_stor.loc[df_stor['storage_id'] == 'Q_GAS_STORAGE', 'capacity_kWh_fuel'].values[0]
+}
+
+rhs_prod = {
+    'P_COAL': df_app.loc[df_app['DER_id'] == 'P_COAL', 'max_power_kW'].values[0]*24,
+    'P_GAS': df_app.loc[df_app['DER_id'] == 'P_GAS', 'max_power_kW'].values[0]*24
+}
+rhs_prod_wind = df_t['Wind_Prod[KWh]'].tolist()
+rhs_prod_pv = df_t['PV_Prod[KWh]'].tolist()
+
+efficiencies = {
+    'eta_COAL': df_app.loc[df_app['DER_id'] == 'P_COAL', 'efficiency'].values[0],
+    'eta_GAS': df_app.loc[df_app['DER_id'] == 'P_GAS', 'efficiency'].values[0]
+}
+
+co2_per_kWh = {
+    'CO2_per_kWh_COAL': df_app.loc[df_app['DER_id'] == 'P_COAL', 'CO2_per_kWh'].values[0],
+    'CO2_per_kWh_GAS': df_app.loc[df_app['DER_id'] == 'P_GAS', 'CO2_per_kWh'].values[0]
+}
+
+min_prod_ratio = {
+    'min_prod_ratio_COAL': df_app.loc[df_app['DER_id'] == 'P_COAL', 'min_power_ratio'].values[0],
+    'min_prod_ratio_GAS': df_app.loc[df_app['DER_id'] == 'P_GAS', 'min_power_ratio'].values[0]
+}
+
+starting_storage_levels = {
+    'Q_COAL_STORAGE': df_stor.loc[df_stor['storage_id'] == 'Q_COAL_STORAGE', 'starting_level_kWh_fuel'].values[0],
+    'Q_GAS_STORAGE': df_stor.loc[df_stor['storage_id'] == 'Q_GAS_STORAGE', 'starting_level_kWh_fuel'].values[0]
+}
+
+input_data = InputData(
+    variables,
+    gas_prices,
+    coal_prices,
+    eua_prices,
+    rhs_demand,
+    rhs_storage,
+    rhs_prod,
+    rhs_prod_wind,
+    rhs_prod_pv,
+    efficiencies,
+    co2_per_kWh,
+    min_prod_ratio,
+    starting_storage_levels
+)
+
+model = DeterministicModel(input_data)
+model.run()
+model.print_results()
